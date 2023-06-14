2fb7ba7597c14b77c555d426c0f94c7afa41994e
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-06-14 21:30:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const li = document.querySelectorAll('li');
+
+for(let i = 0, i < li.length; i++){
+	if(li[i].id){
+		alert(`The level of the element is: ${i+1}`);
+	}
+}
